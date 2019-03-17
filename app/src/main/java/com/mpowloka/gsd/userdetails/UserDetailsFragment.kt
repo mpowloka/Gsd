f16a88118a78bb67bc38c7b76f5645cff6b47524
mpowloka <michalpowloka@interia.pl>
2019-03-17 18:09:26 +0100

    Implement basic user details view with mock data

---
+++ app/src/main/java/com/mpowloka/gsd/userdetails/UserDetailsFragment.kt
@@ -0,0 +1,36 @@
+package com.mpowloka.gsd.userdetails
+
+import android.content.Context
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.mpowloka.gsd.R
+import kotlinx.android.synthetic.main.fragment_user_details.*
+
+class UserDetailsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_user_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        login_tv.text = "Jon Skeet"
+        organization_tv.text = "Google"
+        Glide
+            .with(view.context)
+            .load("https://avatars3.githubusercontent.com/u/583231?v=4")
+            .into(user_picture_iv)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val actionBar = (activity as? AppCompatActivity)?.supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBar?.title = "Jon Skeet"
+    }
+}
